7ddc2c2d6e69db54a3c6fe29003618e10a589433
leviable <levi.noecker@gmail.com>
2019-12-07 11:21:12 -0600

    Update how package is renamed

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(join(HERE, 'README.rst'), 'r') as r:
     long_description = r.read()
 
 setup(
-    name="r${E_COUNT}",
+    name="reeeeeeeeee",
     author="Levi Noecker",
     author_email="levi.noecker@gmail.com",
     url="https://github.com/leviable/reeeeeeeeee",
